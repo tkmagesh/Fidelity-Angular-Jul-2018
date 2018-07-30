7616e1f6c299262847e5b9655dbb693d3ab7ce45
Magesh <tkmagesh77@gmail.com>
2018-07-30 14:18:46 +0530

    Refactoring to 'utilsModule'

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { UtilsModule } from './utils/utils.module';
 
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bugTracker/bugTracker.component';
@@ -9,9 +10,6 @@ import { BugEditComponent } from './bugTracker/views/bugEdit.component';
 
 import { ClosedCountPipe } from './bugTracker/pipes/closedCount.pipe';
 
-import { TrimTextPipe } from './bugTracker/pipes/trimText.pipe';
-import { SortPipe } from './bugTracker/pipes/sort.pipe';
-import { ElapsedPipe } from './bugTracker/pipes/elapsed.pipe';
 
 import { BugOperationsService } from './bugTracker/services/bugOperations.service';
 import { BugStorageService } from './bugTracker/services/bugStorage.service';
@@ -23,13 +21,12 @@ import { BugStorageService } from './bugTracker/services/bugStorage.service';
     , BugStatsComponent
     , BugEditComponent
     , ClosedCountPipe
-    , TrimTextPipe
-    , SortPipe
-    , ElapsedPipe
+    
   ],
   imports: [
     BrowserModule
     , FormsModule
+    , UtilsModule
   ],
   providers: [
     BugOperationsService
